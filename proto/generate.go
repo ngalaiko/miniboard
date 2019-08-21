c47ff0ea6ccedd4339070dc81edceca6939ed3cb
Nikita Galaiko <nikita@galaiko.rocks>
2019-08-21 17:32:40 +0200

    api: rename authentication -> auth

--- proto/generate.go
+++ proto/generate.go
@@ -1,5 +1,5 @@
 package proto // import "miniflux.app/proto"
 
 //go:generate protoc -I./third_party/googleapis -I. --go_out=plugins=grpc:. ./users/v1/users_service.proto
-//go:generate protoc -I./third_party/googleapis -I. --go_out=plugins=grpc:. ./authentications/v1/authentication_service.proto
+//go:generate protoc -I./third_party/googleapis -I. --go_out=plugins=grpc:. ./auth/v1/auth_service.proto
 //go:generate protoc -I./third_party/googleapis -I. --go_out=plugins=grpc:. ./users/articles/v1/articles_service.proto
