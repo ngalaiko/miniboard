79c3bda67c7f6836321b04b28b0fdff7ce109488
Nikita Galaiko <nikita@galaiko.rocks>
2019-08-21 21:47:25 +0200

    api: start gRPC server

--- api/server.go
+++ api/server.go
@@ -1,11 +1,47 @@
 package api // import "miniboard.app/api"
 
-import "context" // Server is the api server.
+import (
+	"context"
+	"net"
+
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	usersservice "miniboard.app/application/api/users"
+	"miniboard.app/application/proto/users/v1"
+	"miniboard.app/application/storage"
+)
 
 // Server is the api server.
-type Server struct{}
+type Server struct {
+	grpcServer *grpc.Server
+}
 
 // NewServer creates new api server.
-func NewServer(ctx context.Context) *Server {
-	return &Server{}
+func NewServer(ctx context.Context, db storage.DB) *Server {
+	grpcServer := grpc.NewServer()
+
+	users.RegisterUsersServiceServer(grpcServer, usersservice.New(db))
+
+	reflection.Register(grpcServer)
+
+	return &Server{
+		grpcServer: grpcServer,
+	}
+}
+
+// Serve starts the server.
+func (s *Server) Serve(ctx context.Context, lis net.Listener) error {
+	logrus.Infof("starting gRPC server on %s", lis.Addr())
+
+	if err := s.grpcServer.Serve(lis); err != nil {
+		return errors.Wrap(err, "failed to start gRPC server")
+	}
+	go func() {
+		<-ctx.Done()
+		s.grpcServer.GracefulStop()
+	}()
+
+	return nil
 }
--- cmd/miniboard/main.go
+++ cmd/miniboard/main.go
@@ -2,13 +2,43 @@ package main // import "miniflux.app/cmd"
 
 import (
 	"context"
+	"flag"
+	"net"
+	"os"
 
-	"miniboard.app/application"
+	"github.com/sirupsen/logrus"
+	"miniboard.app/application/api"
+	"miniboard.app/application/storage/bolt"
+)
+
+var (
+	boltPath = flag.String("bolt-path", "./bolt.db", "Path to the bolt storage.")
+	addr     = flag.String("addr", ":8080", "Address to listen for connections.")
 )
 
 func main() {
+	flag.Parse()
+
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	_ = application.New(ctx)
+	file, err := os.OpenFile(*boltPath, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		logrus.Panicf("failed to open bolt file: %s", err)
+	}
+
+	db, err := bolt.New(ctx, file.Name())
+	if err != nil {
+		logrus.Panicf("failed to create bolt database: %s", err)
+	}
+
+	lis, err := net.Listen("tcp", *addr)
+	if err != nil {
+		logrus.Panicf("failed to open a connection: %s", err)
+	}
+
+	server := api.NewServer(ctx, db)
+	if err := server.Serve(ctx, lis); err != nil {
+		logrus.Panicf("failed to start the server: %s", err)
+	}
 }
