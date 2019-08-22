53b37606b258602cc787ef702ac25429211bafce
Nikita Galaiko <nikita@galaiko.rocks>
2019-08-22 14:11:19 +0200

    generate: rm

--- proto/generate.go
+++
@@ -1,5 +0,0 @@
-package proto // import "miniflux.app/proto"
-
-//go:generate protoc -I./third_party/googleapis -I. --go_out=plugins=grpc:. ./users/v1/users_service.proto
-//go:generate protoc -I./third_party/googleapis -I. --go_out=plugins=grpc:. ./users/articles/v1/articles_service.proto
-//go:generate protoc -I./third_party/googleapis -I. --go_out=plugins=grpc:. ./users/authentications/v1/authentications_service.proto
