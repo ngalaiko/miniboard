04bcd6d20efc16818c9e76cb3df9a0a53aa567ff
Nikita Galaiko <nikita@galaiko.rocks>
2019-08-25 20:20:44 +0200

    web: decided on router component

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,11 +1,19 @@
+<svelte:window on:pushstate={x => pathname = x.target.location.pathname} on:popstate={x => pathname = x.target.location.pathname}/>
+
 <script>
     import Login from './pages/login/Login.svelte';
+    import NotFound from './pages/notfound/NotFound.svelte';
     import { Api } from './components/api/api';
+    import navid from "./components/navaid/navaid";
 
     let api = new Api();
+    let router = navid();
+
+    let pathname = location.pathname;
 </script>
 
 <div class="app">
+   // todo: router component
   <Login api={api} />
 </div>
 
--- web/web.go
+++ web/web.go
@@ -6,5 +6,20 @@ import (
 
 // Handler returns http handler for the UI.
 func Handler() http.Handler {
-	return http.FileServer(http.Dir("web/src"))
+	return http.FileServer(&fs{
+		rootFS: http.Dir("web/src"),
+	})
+}
+
+type fs struct {
+	rootFS http.FileSystem
+}
+
+// Open opens a file if it exists, or 'index.html' otherweise.
+func (fs *fs) Open(name string) (http.File, error) {
+	file, err := fs.rootFS.Open(name)
+	if err != nil {
+		return fs.rootFS.Open("index.html")
+	}
+	return file, err
 }
