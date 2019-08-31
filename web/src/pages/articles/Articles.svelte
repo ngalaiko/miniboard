10ca93eac375bf71abd175bb24b86c3138ae3c01
Nikita Galaiko <nikita@galaiko.rocks>
2019-08-31 08:26:12 +0200

    web: add article to the list

--- web/src/pages/articles/Articles.svelte
+++ web/src/pages/articles/Articles.svelte
@@ -7,16 +7,33 @@
     export let user
 
     let articlesService = new ArticlesService(api, user)
-    let articles = articlesService.next()
+
+    let articlesList = []
+    articlesService.next()
+        .then(list => { articlesList = articlesList.concat(list) })
+
+    let url = ""
+    let error = ""
+    function handleAdd() {
+        error = ""
+        articlesService.add(url)
+            .catch(err => { error = err })
+            .then(article => { articlesList = [article].concat(articlesList) } )
+            .then(() => { url = "" })
+    }
 </script>
 
 <div>
-    <ArticlesForm api={api} user={user} />
-    {#await articles}
-        loading articles...
-    {:then articles}
-        {#each articles as article}
+    <div>
+        <input type="text" bind:value={url} placeholder="add" required="" />
+        {#if (error != "")}
+            <div class="alert">{error}</div>
+        {/if}
+        <button on:click|preventDefault={handleAdd}>Add</button>
+    </div>
+    <div>
+        {#each articlesList as article}
             <Article {...article} />
         {/each}
-    {/await}
+    </div>
 </div>
--- web/src/pages/articles/articles-service.js
+++ web/src/pages/articles/articles-service.js
@@ -7,11 +7,31 @@ export class ArticlesService {
     this.items = []
   }
 
+  // next returns a Promise with articles.
   next() {
     return this.api.get(`/api/v1/${this.user.name}/articles?page_size=${this.pageSize}&from=${this.from}`)
     .then(resp => { 
       this.from = resp.page_token 
       return resp.articles
     })
+  }
+
+  // create returns a Promise with the article.
+  add(url) {
+    return this.api.post(`/api/v1/${this.user.name}/articles`, {
+      url: url
+    })
+    .then(this.ifError)
+  }
+
+  ifError(resp) {
+    if (resp.error === undefined) {
+      return resp
+    }
+    if (resp.code !== undefined) {
+      throw resp.message
+    }
+    console.error(resp)
+    throw "something went wrong, try again"
   }
 }
