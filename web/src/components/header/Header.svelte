002bd670f7357240c979f3361e0c08f142d49653
Nikita Galaiko <nikita@galaiko.rocks>
2019-08-31 15:31:09 +0200

    web: style for header

--- web/src/components/header/Header.svelte
+++ web/src/components/header/Header.svelte
@@ -8,5 +8,28 @@
 </script>
 
 <div>
-    <div on:click={onLogout}>Logout</div>
+    <button on:click|preventDefault={onLogout}>Logout</button>
 </div>
+
+<style>
+  div {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+
+  button {
+      display: inline-block;
+      -webkit-appearance: none;
+      -moz-appearance: none;
+      font-size: 1.1em;
+      cursor: pointer;
+      padding: 3px 10px;
+      border: 1px solid;
+      border-radius: unset;
+  }
+  
+  button:hover, .button:focus {
+      border-color: #2f5bb7;
+      outline-width: 0
+  }
+</style>
