e21e4bd5093dc6de31281a66eae981d9ae6596ac
Nikita Galaiko <nikita@galaiko.rocks>
2019-09-04 11:47:21 +0200

    web: labels cache

--- web/src/pages/articles/labels-service.js
+++ web/src/pages/articles/labels-service.js
@@ -1,16 +1,44 @@
 export class LabelsService {
   constructor(api, user) {
     this.api = api
-    this.user= user
+    this.user = user
+    this.titleToLabel = {}
+    this.nameToLabel = {}
+
+    // todo: list all in smaller batches
+    this.api.get(`/api/v1/${this.user.name}/labels?page_size=100`)
+      .then(resp => resp.labels.forEach(label => {
+        this.saveLabel(label)
+      }))
   }
 
   create(title) {
+    let known = this.titleToLabel[title]
+    if (known !== undefined) {
+      return new Promise(resolve => resolve(known))
+    }
     return this.api.post(`/api/v1/${this.user.name}/labels`, {
       title: title,
+    }).then(label => {
+      this.saveLabel(label)
+      return label
     })
   }
 
   get(labelName) {
+    let known = this.nameToLabel[labelName]
+    if (known !== undefined) {
+      return new Promise(resolve => resolve(known))
+    }
     return this.api.get(`/api/v1/${labelName}`)
+      .then(label => {
+        this.saveLabel(label)
+        return label
+      })
+  }
+
+  saveLabel(label) {
+    this.titleToLabel[label.title] = label
+    this.nameToLabel[label.name] = label
   }
 }
