a832e12b41080a83d067a80498d4faa2ad1609d9
Nikita Galaiko <nikita@galaiko.rocks>
2019-09-17 21:52:49 +0200

    split ci to build and test

--- stamp.sh
+++ stamp.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-set -euo pipefail
 
-echo "MINIBOARD_VERSION $(git rev-parse HEAD)"
+set -eo pipefail
+
+if [ -z ${VERSION} ]; then
+    VERSION=$(git rev-parse --short HEAD)_$(uname -s)_$(uname -m)
+fi
+
+echo "MINIBOARD_VERSION ${VERSION}"
