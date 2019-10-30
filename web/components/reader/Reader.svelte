552acfe140097614c86a9e228bae716b9b344db0
Nikita Galaiko <nikita@galaiko.rocks>
2019-10-30 19:41:52 +0100

    more style

--- web/components/reader/Reader.svelte
+++ web/components/reader/Reader.svelte
@@ -14,7 +14,7 @@
     }
 </script>
 
-<div class='reader'>
+<div class='article'>
     {#await articles.get(name)}
         loading...
     {:then article}
@@ -24,8 +24,10 @@
                 {window.open(article.url, '_blank')}
             </div>
         {:else}
-            <h1>{article.title}</h1>
-            {@html b64DecodeUnicode(article)}
+            <div class='page'>
+                <h1>{article.title}</h1>
+                {@html b64DecodeUnicode(article)}
+            </div>
         {/if}
     {:catch e}
         failed to fetch, are you online?
@@ -33,10 +35,34 @@
 </div>
 
 <style>
-    .reader{
-        overflow: auto;
+    .article {
+        min-height: 100%;
+        margin: 22px auto 0 auto;
+        overflow: visible;
         font-size: 18px;
         line-height: 29px;
+    }
+
+    .page {
+        text-align: start;
+        word-wrap: break-word;
+        padding-top: 53px;
+        margin-top: 0;
+        border-top: none;
+        padding-left: 0px;
+        padding-right: 0px;
+        margin-left: 70px;
+        margin-right: 70px;
+        padding-bottom: 45px;
+        max-width: 83.2ex;
+    }
+
+    h1 {
+        font-weight: bold;
+        font-size: 1.95552em;
+        line-height: 1.2141em;
+        margin-top: 0;
+        margin-bottom: 0.5em;
     }
 
     :global(pre) {
