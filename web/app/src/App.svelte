f3f94ae59673e4fa75799a8c7374c4e73e1b1bf2
Nikita Galaiko <nikita@galaiko.rocks>
2019-12-21 15:44:58 +0100

    move web modules to subdirs

--- web/app/src/App.svelte
+++ web/app/src/App.svelte
@@ -62,7 +62,7 @@
 
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/serviceWorker.js').then((registration) => {
+            navigator.serviceWorker.register('/service_worker/service_worker.js').then((registration) => {
                 console.log('ServiceWorker registration successful')
             }, (err) => {
                 console.log(`ServiceWorker registration failed: ${err}`)
