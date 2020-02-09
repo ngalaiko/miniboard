cf6291c7ba32589fef37a4ebb2970b3da15bdeb0
Nikita Galaiko <nikita@galaiko.rocks>
2020-02-09 17:15:27 +0100

    log message when emails disabled

--- server/email/disabled/email.go
+++ server/email/disabled/email.go
@@ -1,8 +1,6 @@
 package disabled
 
 import (
-	"errors"
-
 	"github.com/sirupsen/logrus"
 )
 
@@ -18,7 +16,11 @@ func New() *Client {
 
 // Send sends _payload_ to the client.
 func (c *Client) Send(to string, subject string, payload string) error {
-	return errors.New("email client disabled")
+	log("email").Warningf(`to: %s
+%s
+
+%s`, to, subject, payload)
+	return nil
 }
 
 func log(src string) *logrus.Entry {
