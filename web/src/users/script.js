f00143e0732597f52dee51b7ddd90ca36b6ba1a8
Nikita Galaiko <nikita@galaiko.rocks>
2020-07-04 12:46:47 +0200

    web: add articles

--- web/src/users/script.js
+++ web/src/users/script.js
@@ -13,33 +13,126 @@ const setCurrentUser = async () => {
     }
 }
 
-const feedsContainer = document.getElementById('feeds-container')
+//
+
+const addToggle = document.getElementById('add-toggle')
+const addForm = document.getElementById('add-form')
+
+const handleAddToggle = async (e) => {
+    e.target.checked
+        ? addForm.classList.remove("hidden")
+        : addForm.classList.add("hidden")
+}
+
+addToggle.addEventListener('click', handleAddToggle)
+
+//
+
+const addFormButton = document.getElementById('add-form-button')
+const addFormInput = document.getElementById('add-form-input')
+
+const handleAddFormButton = async (e) => {
+    e.preventDefault()
+
+    await handleAdd(addFormInput.value)
+
+    addFormInput.value = ''
+}
+
+const handleAdd = async (url) => {
+    let response = await fetch(`/api/v1/${username}/sources`, {
+        method: 'POST',
+        body: JSON.stringify({
+            url: url
+        })
+    })
+    switch (response.status) {
+        case 200:
+            let resourceResponse = await fetch(`/api/v1/${((await response.json()).name)}`)
+            let resource = await resourceResponse.json()
+            switch (resource.name.split('/')[2]) {
+                case "articles":
+                    addArticle(resource)
+                    break
+                case "feeds":
+                    break
+            }
+            break
+        default:
+            alert(`Error: ${(await response.json()).message}`)
+            break
+    }
+}
+
+addFormButton.addEventListener('click', handleAddFormButton)
+
+//
+
+const feedsList = document.getElementById('feeds-list')
 
 const loadFeeds = async () => {
     let response = await fetch(`/api/v1/${username}/feeds`)
-    // todo: handle error
+    if (response.status !== 200) {
+        alert(`Error: ${(await response.json()).message}`)
+        return
+    }
+
     let body = (await response.json())
     let nextPageToken = body.nextPageToken
     let feeds = body.feeds
 
     if (nextPageToken === '' && feeds.length == 0) {
-        feedsContainer.innerText = 'Empty'
+        feedsList.innerText = 'Empty'
+        return
     }
 }
 
-const articlesContainer = document.getElementById('articles-container')
+//
+
+const articlesList = document.getElementById('articles-list')
 
 const loadArticles = async () => {
-    let response = await fetch(`/api/v1/${username}/articles`)
-    // todo: handle error
+    let response = await fetch(`/api/v1/${username}/articles?page_size=10`)
+    if (response.status !== 200) {
+        alert(`Error: ${(await response.json()).message}`)
+        return
+    }
+
     let body = (await response.json())
     let nextPageToken = body.nextPageToken
     let articles = body.articles
 
     if (nextPageToken === '' && articles.length == 0) {
-        articlesContainer.innerText = 'Empty'
+        articlesList.innerText = 'Empty'
+        return
     }
+
+    articlesList.innerText = ''
+    articles.forEach(addArticle)
 }
+
+const addArticle = (article) => {
+    let li = document.createElement('li')
+    li.id = `${article.name}-container`
+    li.innerHTML = `
+    <div id="${article.name}">
+        ${article.title}
+    </div>`
+
+    let child = articlesList.firstChild
+    if (child === null) {
+        articlesList.appendChild(li)
+        return
+    }
+
+    while (child && li.id < child.firstElementChild.id) {
+        child = child.nextSibling
+    }
+
+    articlesList.insertBefore(li, child)
+}
+
+//
 
 const init = async () => {
     await setCurrentUser()
