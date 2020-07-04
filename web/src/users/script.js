55c9be5de4b2a65fd93e0a65c5ad64e62d28348c
Nikita Galaiko <nikita@galaiko.rocks>
2020-07-04 02:16:39 +0200

    web: load lists

--- web/src/users/script.js
+++ web/src/users/script.js
@@ -1,6 +1,6 @@
 let username = undefined
 
-const init = async () => {
+const setCurrentUser = async () => {
     let response = await fetch('/api/v1/users/me')
     switch (response.status / 100) {
     case 2:
@@ -11,6 +11,40 @@ const init = async () => {
         document.location = '/'
         break
     }
+}
+
+const feedsContainer = document.getElementById('feeds-container')
+
+const loadFeeds = async () => {
+    let response = await fetch(`/api/v1/${username}/feeds`)
+    // todo: handle error
+    let body = (await response.json())
+    let nextPageToken = body.nextPageToken
+    let feeds = body.feeds
+
+    if (nextPageToken === '' && feeds.length == 0) {
+        feedsContainer.innerText = 'Empty'
+    }
+}
+
+const articlesContainer = document.getElementById('articles-container')
+
+const loadArticles = async () => {
+    let response = await fetch(`/api/v1/${username}/articles`)
+    // todo: handle error
+    let body = (await response.json())
+    let nextPageToken = body.nextPageToken
+    let articles = body.articles
+
+    if (nextPageToken === '' && articles.length == 0) {
+        articlesContainer.innerText = 'Empty'
+    }
+}
+
+const init = async () => {
+    await setCurrentUser()
+    loadFeeds()
+    loadArticles()
 }
 
 init()
