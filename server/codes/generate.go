521a597469da3355494f0d8bc4ff8f3e3e5441c2
Nikita Galaiko <nikita@galaiko.rocks>
2020-09-01 13:53:54 +0200

    server: fix generate

--- server/codes/generate.go
+++ server/codes/generate.go
@@ -1,3 +1,3 @@
 package codes
 
-//go:generate protoc -I=../../../proto/codes/v1 -I=../../../proto/third_party ../../../proto/codes/v1/codes_service.proto --go_out=plugins=grpc:. --grpc-gateway_out=:.
+//go:generate protoc -I=../../proto/codes/v1 -I=../../proto/third_party ../../proto/codes/v1/codes_service.proto --go_out=plugins=grpc:. --grpc-gateway_out=:.
