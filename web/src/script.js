6b36fb5a1d1526260780c531211ca3f1ae5efcc1
Nikita Galaiko <nikita@galaiko.rocks>
2020-09-21 18:54:01 +0200

    web: rm get user

--- web/src/script.js
+++ web/src/script.js
@@ -32,18 +32,7 @@ const handleCodeClick = async (e) => {
     document.location = `/codes?code=${codeInput.value}`
 }
 
-const init = async () => {
-    let response = await fetch('/api/v1/users/me')
-    if (response.status != 200) {
-        return
-    }
-
-    document.location = '/users'
-}
-
 emailButton.addEventListener('click', handleEmailClick)
 codeButton.addEventListener('click', handleCodeClick)
 
 emailInput.addEventListener('input', handleEmailInput)
-
-init()
