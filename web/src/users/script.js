8e9dbefd62ad6b0dc22a44534191ad15f40d9a6c
Nikita Galaiko <nikita@galaiko.rocks>
2020-09-29 18:23:13 +0200

    web: switch between articles and feeds

--- web/src/users/script.js
+++ web/src/users/script.js
@@ -1,11 +1,34 @@
-let articleReader = document.querySelector('#article-reader')
-let articleList = document.querySelector('#article-list')
-let feedList = document.querySelector('#feed-list')
+const feedList = document.querySelector('#feed-list')
+
+const articlesContainer = document.querySelector('#articles-container')
+const articlesBackButton = document.querySelector('#articles-back-button')
+const articleList = document.querySelector('#article-list')
+
+const articleReader = document.querySelector('#article-reader')
+
+const showArticles = () => {
+    feedList.classList.add('hidden')
+    articlesContainer.classList.remove('hidden')
+}
+
+const showFeeds = () => {
+    feedList.classList.remove('hidden')
+    articlesContainer.classList.add('hidden')
+}
 
 const loadState = () => {
     const urlParams = new URLSearchParams(window.location.search.slice(1))
-    articleList.setAttribute('feed', urlParams.get('feed'))
-    articleReader.setAttribute('article', urlParams.get('article'))
+    const feedId = urlParams.get('feed')
+    const articleId = urlParams.get('article')
+
+    articleList.setAttribute('feed', feedId)
+    articleReader.setAttribute('article', articleId)
+
+    if (feedId) {
+        showArticles()
+    } else {
+        showFeeds()
+    }
 }
 
 const storeState = (key, value) => {
@@ -21,6 +44,17 @@ const storeState = (key, value) => {
     window.history.pushState({ path: refresh }, '', refresh)
 }
 
+const deleteState = (key) => {
+    const urlParams = new URLSearchParams(window.location.search.slice(1))
+
+    urlParams.delete(key)
+
+    let refresh = window.location.protocol +
+        "//" + window.location.host + window.location.pathname +
+        `?${urlParams.toString()}`
+    window.history.pushState({ path: refresh }, '', refresh)
+}
+
 articleList.addEventListener('ArticleSelected', (e) => {
     articleReader.setAttribute('article', e.detail.id)
     storeState('article', e.detail.id)
@@ -29,6 +63,14 @@ articleList.addEventListener('ArticleSelected', (e) => {
 feedList.addEventListener('FeedSelected', (e) => {
     articleList.setAttribute('feed', e.detail.id)
     storeState('feed', e.detail.id)
+
+    showArticles()
+})
+
+articlesBackButton.addEventListener('click', (e) => {
+    articleList.removeAttribute('feed')
+    deleteState('feed')
+    showFeeds()
 })
 
 window.addEventListener('popstate', loadState)
