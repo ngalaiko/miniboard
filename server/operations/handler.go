6fa2175d2eaa0cfee61589e64bb8c8c7b4ecd119
Nikita Galaiko <nikita@galaiko.rocks>
2021-01-07 21:46:17 +0100

    added operations handler

---
+++ server/operations/handler.go
@@ -0,0 +1,65 @@
+package operations
+
+import (
+	"errors"
+	"net/http"
+	"regexp"
+
+	"github.com/ngalaiko/miniboard/server/authorizations"
+	"github.com/ngalaiko/miniboard/server/httpx"
+)
+
+// Handler handles http requests for user resource.
+type Handler struct {
+	service *Service
+	logger  logger
+}
+
+// NewHandler creates a new handler for operations resource.
+func NewHandler(service *Service, logger logger) *Handler {
+	return &Handler{
+		service: service,
+		logger:  logger,
+	}
+}
+
+// ServeHTTP implements http.Handler.
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h.handleGet().ServeHTTP(w, r)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
+func (h *Handler) handleGet() http.Handler {
+	getOperation := regexp.MustCompile(`/(.*)$`)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		m := getOperation.FindStringSubmatch(r.URL.Path)
+		if len(m) == 0 {
+			http.NotFound(w, r)
+			return
+		}
+		h.handleGetOperation(w, r, m[1])
+	})
+}
+
+func (h *Handler) handleGetOperation(w http.ResponseWriter, r *http.Request, id string) {
+	token, auth := authorizations.FromContext(r.Context())
+	if !auth {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	operation, err := h.service.Get(r.Context(), id, token.UserID)
+	switch {
+	case err == nil:
+		httpx.JSON(w, h.logger, operation)
+	case errors.Is(err, errNotFound):
+		http.NotFound(w, r)
+	default:
+		h.logger.Error("failed to get operation: %s", err)
+		httpx.InternalError(w, h.logger)
+	}
+}
---
+++ server/operations/handler_test.go
@@ -0,0 +1,123 @@
+package operations
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ngalaiko/miniboard/server/authorizations"
+)
+
+func Test_Handler__Post(t *testing.T) {
+	ctx := context.Background()
+	logger := &testLogger{}
+	service := NewService(ctx, logger, testDB(ctx, t), nil)
+	handler := NewHandler(service, logger)
+
+	req, err := http.NewRequest("Post", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusMethodNotAllowed {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusMethodNotAllowed)
+	}
+}
+
+func Test_Handler__Get_unauthorized(t *testing.T) {
+	ctx := context.Background()
+	logger := &testLogger{}
+	service := NewService(ctx, logger, testDB(ctx, t), nil)
+	handler := NewHandler(service, logger)
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusUnauthorized)
+	}
+}
+
+func Test_Handler__Get_not_found(t *testing.T) {
+	ctx := context.Background()
+	logger := &testLogger{}
+	db := testDB(ctx, t)
+	service := NewService(ctx, logger, db, nil)
+	handler := NewHandler(service, logger)
+
+	req, err := http.NewRequest("GET", "/404", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req = req.WithContext(authorizations.NewContext(ctx, &authorizations.Token{
+		UserID: "user",
+	}))
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusNotFound {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusNotFound)
+	}
+}
+
+func Test_Handler__Get_found(t *testing.T) {
+	ctx := context.Background()
+	logger := &testLogger{}
+	db := testDB(ctx, t)
+	service := NewService(ctx, logger, db, nil)
+	handler := NewHandler(service, logger)
+
+	operation, err := service.Create(ctx, "user", func(context.Context, *Operation, chan<- *Operation) error {
+		return nil
+	})
+	if err != nil {
+		t.Fatalf("failed to create operation %s", err)
+	}
+
+	req, err := http.NewRequest("GET", "/"+operation.ID, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req = req.WithContext(authorizations.NewContext(ctx, &authorizations.Token{
+		UserID: "user",
+	}))
+	rr := httptest.NewRecorder()
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	returned := &Operation{}
+	if err := json.Unmarshal(rr.Body.Bytes(), &returned); err != nil {
+		t.Fatalf("failed to unmarshal response body: %s", err)
+	}
+
+	if operation.ID != returned.ID {
+		t.Errorf("expected id %s, got %s", operation.ID, returned.ID)
+	}
+
+	if operation.UserID != "user" {
+		t.Errorf("expected user id %s, got %s", "user", returned.UserID)
+	}
+}
