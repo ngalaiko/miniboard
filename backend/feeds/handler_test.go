a130cd644f7cc35358029e7a434f46bfd160afab
Nikita Galaiko <nikita@galaiko.rocks>
2021-01-17 17:06:08 +0100

    backend: reduce duplication

--- backend/feeds/handler_test.go
+++ backend/feeds/handler_test.go
@@ -13,33 +13,55 @@ import (
 	"github.com/ngalaiko/miniboard/backend/operations"
 )
 
-func Test_Handler__Get_invalid_page_size(t *testing.T) {
+func Test_Handler__Get(t *testing.T) {
 	ctx := context.Background()
 	logger := &testLogger{}
 	db := createTestDB(ctx, t)
 	service := NewService(db, &testCrawler{})
 	handler := NewHandler(service, logger, &testOperationsService{})
 
-	req, err := http.NewRequest("GET", "/?page_size=invalid", nil)
-	if err != nil {
-		t.Fatal(err)
+	tc := []struct {
+		URL        string
+		Error      error
+		StatusCode int
+	}{
+		{
+			URL:        "/?page_size=invalid",
+			Error:      errInvalidPageSize,
+			StatusCode: http.StatusBadRequest,
+		},
+		{
+			URL:        "/?before=invalid",
+			Error:      errInvalidBefore,
+			StatusCode: http.StatusBadRequest,
+		},
 	}
 
-	req = req.WithContext(authorizations.NewContext(ctx, &authorizations.Token{
-		UserID: "user",
-	}))
-	rr := httptest.NewRecorder()
-	handler.ServeHTTP(rr, req)
+	for _, testCase := range tc {
+		t.Run(testCase.URL, func(t *testing.T) {
+			req, err := http.NewRequest("GET", testCase.URL, nil)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("handler returned wrong status code: got %v want %v",
-			status, http.StatusBadRequest)
-	}
+			req = req.WithContext(authorizations.NewContext(ctx, &authorizations.Token{
+				UserID: "user",
+			}))
+			rr := httptest.NewRecorder()
+			handler.ServeHTTP(rr, req)
 
-	expected := fmt.Sprintf(`{"message":"%s"}`, errInvalidPageSize)
-	if rr.Body.String() != expected {
-		t.Errorf("handler returned unexpected body: got %v want %v",
-			rr.Body.String(), expected)
+			expectedCode := testCase.StatusCode
+			if status := rr.Code; status != expectedCode {
+				t.Errorf("handler returned wrong status code: got %v want %v",
+					status, expectedCode)
+			}
+
+			expected := fmt.Sprintf(`{"message":"%s"}`, testCase.Error)
+			if rr.Body.String() != expected {
+				t.Errorf("handler returned unexpected body: got %v want %v",
+					rr.Body.String(), expected)
+			}
+		})
 	}
 }
 
