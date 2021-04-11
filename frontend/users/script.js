c2ddfd67e2b2a55872755b6af8218d01caee80d2
Nikita Galaiko <nikita@galaiko.rocks>
2021-04-11 11:23:56 +0200

    frontend: fix toast

--- frontend/users/script.js
+++ frontend/users/script.js
@@ -302,10 +302,10 @@ const renderToastMessage = (promise, message, onSuccess) => {
 }
 
 const renderToast = (promise, message, onSuccess) => `
-    <div class="toast-container show">
+    <div class="toast-container">
         ${renderToastMessage(promise, message, onSuccess)}
         <button class="toast-button" type="button" onclick="const parent = this.parentNode; parentNode.remove();">
-            <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <svg viewBox="0 0 24 24" width="20" height="20" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                 <line x1="18" y1="6" x2="6" y2="18"></line>
                 <line x1="6" y1="6" x2="18" y2="18"></line>
             </svg>
